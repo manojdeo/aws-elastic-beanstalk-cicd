ba597df826be231926af37cc88b4f83a9650a842
manojdeo <manojdeo@gmail.com>
2020-09-17 23:23:31 +0530

    First Change

---
+++ src/main/java/com/manoj/aws/AwsElasticBeanstalkCicdApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkCicdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/manoj/aws/HelloResource.java
@@ -0,0 +1,15 @@
+package com.manoj.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloResource {
+	@GetMapping
+	public String hello() {
+		return "Hello message";
+	}
+
+}
